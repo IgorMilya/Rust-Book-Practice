e7c2d400626e61b2a22bc952006c5252280339bc
imilay11 <imilay11@gmail.com>
2023-11-27 22:08:46 +0200

    Structs and impl block

---
+++ onwnershiping/target/debug/deps/onwnershiping.d
@@ -0,0 +1,5 @@
+I:\Rust\onwnershiping\target\debug\deps\onwnershiping.exe: src\main.rs
+
+I:\Rust\onwnershiping\target\debug\deps\onwnershiping.d: src\main.rs
+
+src\main.rs:
---
+++ onwnershiping/target/debug/onwnershiping.d
@@ -0,0 +1,1 @@
+I:\Rust\onwnershiping\target\debug\onwnershiping.exe: I:\Rust\onwnershiping\src\main.rs
