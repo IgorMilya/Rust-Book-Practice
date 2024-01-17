5588c4ba56c5c2a42e0a33978fa4c02460e53113
imilay11 <imilay11@gmail.com>
2024-01-17 15:58:25 +0200

    Closures

---
+++ closures/src/main.rs
@@ -0,0 +1,27 @@
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut iterys = Counter::new();
+    let o = &iterys.next();
+}
