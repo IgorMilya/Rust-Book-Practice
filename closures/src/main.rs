956dc0f5c878a270fe334d75f29e1872debe1af0
imilay11 <imilay11@gmail.com>
2024-01-23 01:25:58 +0200

    Concurrency

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,27 +1,11 @@
-struct Counter {
-    count: u32,
+#[macro_use] extern crate rocket;
+
+#[get("/get")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-impl Counter {
-    fn new() -> Self {
-        Self { count: 0 }
-    }
-}
-
-impl Iterator for Counter {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < 5 {
-            self.count += 1;
-            Some(self.count)
-        } else {
-            None
-        }
-    }
-}
-
-fn main() {
-    let mut iterys = Counter::new();
-    let o = &iterys.next();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,3 +1,55 @@
+use ::std::thread;
+use std::sync::mpsc;
+// use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // Using Threads to Run Code Simultaneously
+    // let handle = thread::spawn(|| {
+    //     for i in 0..10 {
+    //         println!("hi number {} from the spawned thread!", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+    //
+    // for i in 0..5 {
+    //     println!("hi number {} from the main thread!", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+    // handle.join().unwrap();
+    //
+    // let v = vec![1, 2, 3];
+    //
+    // let handle = thread::spawn(|| {
+    //     println!("Here's a vector: {:?}", v);
+    // });
+    //
+    // handle.join().unwrap();
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Using Message Passing to Transfer Data Between Threads
+
+
+    fn main() {
+        let (tx, rx) = mpsc::channel();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
