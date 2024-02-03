bb585efa94be1f7d57cedddd3b8c1f2bf4fb0a2a
imilay11 <imilay11@gmail.com>
2024-02-03 19:24:44 +0200

    Done with concurrency

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,6 +1,8 @@
 use ::std::thread;
 use std::sync::mpsc;
-// use std::time::Duration;
+use std::time::Duration;
+use::std::sync::{Mutex, Arc};
+use std::rc::Rc;
 
 fn main() {
     // Using Threads to Run Code Simultaneously
@@ -29,24 +31,63 @@ fn main() {
 
     // Using Message Passing to Transfer Data Between Threads
 
+    // let (tx, rx) = mpsc::channel();
+    // let tx2 = tx.clone();
+    //
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("hi"),
+    //         String::from("from"),
+    //         String::from("the"),
+    //         String::from("thread"),
+    //     ];
+    //
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+    //
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("more"),
+    //         String::from("messages"),
+    //         String::from("for"),
+    //         String::from("you"),
+    //     ];
+    //
+    //     for val in vals {
+    //         tx2.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+    //
+    // for received in rx {
+    //     println!("Got: {}", received);
+    // }
 
-    fn main() {
-        let (tx, rx) = mpsc::channel();
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Shared-State Concurrency
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    println!("Result: {}", *counter.lock().unwrap());
 
 
 
